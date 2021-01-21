7e1b3d5af1f285a668d4181807b0a01bbf7bfdd5
Musa FINDIK <Musa FINDIK@MUSAFINDIK>
2021-01-21 22:05:23 +0100

    21 Ocak 2021 4.Günün Ödevi

---
+++ DorduncuGununOdevi/MyDictinory.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DorduncuGununOdevi
+{
+    class MyDictinory<Plaka, Sehir>
+    {
+        Plaka[] plakalar;
+        Sehir[] sehirler;
+        public MyDictinory()
+        {
+            plakalar = new Plaka[0];
+            sehirler = new Sehir[0];
+        }
+        public void Add(Plaka plaka, Sehir sehir)
+        {
+            Plaka[] tempPlaka = plakalar;
+            Sehir[] tempSehir = sehirler;
+            plakalar = new Plaka[plakalar.Length + 1];
+            sehirler = new Sehir[sehirler.Length + 1];
+            for (int i = 0; i < tempPlaka.Length; i++)
+            {
+                plakalar[i] = tempPlaka[i];
+                sehirler[i] = tempSehir[i];
+            }
+            plakalar[plakalar.Length - 1] = plaka;
+            sehirler[sehirler.Length - 1] = sehir;
+        }
+
+        public (Plaka, Sehir)[] Items 
+        { 
+            get 
+            {
+                List<(Plaka, Sehir)> items = new List<(Plaka, Sehir)>();
+                for (int i = 0; i < sehirler.Length; i++) 
+                {
+                    items.Add((plakalar[i], sehirler[i]));
+                }
+                return items.ToArray();
+            } 
+        }
+
+        //Aşağıdaki kod istediğim gibi sonuç vermediğinden internetten yardım ile yokarıdaki kodu oluşturdum
+
+        //public Plaka[] Plakalar 
+        //{ 
+        //    get { return plakalar; } 
+        //}
+
+        // public Sehir[] Sehirler 
+        //{ 
+        //    get { return sehirler; } 
+        //}
+
+    }
+}
---
+++ DorduncuGununOdevi/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DorduncuGununOdevi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictinory<int, string> sehirler = new MyDictinory<int, string>();
+            sehirler.Add(1, "Adana");
+            sehirler.Add(6, "Ankara");
+            sehirler.Add(18, "Çankırı");
+            sehirler.Add(21, "Diyarbakır");
+            foreach (var p in sehirler.Items)
+            {
+                //Console.WriteLine($"{p.Item2} : {p.Item1}");
+                Console.WriteLine("Plaka : " + p.Item1 + " Şehir : " + p.Item2);
+            }
+     
+        }
+    }
+}
